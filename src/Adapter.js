f565264c2e0da1ba3a2b52801d28fb188a107463
tsigel <tsddaniil@gmail.com>
2018-06-28 18:13:35 +0300

    Remove compiled files

--- src/Adapter.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Adapter = /** @class */ (function () {
-    function Adapter() {
-    }
-    return Adapter;
-}());
-exports.Adapter = Adapter;
-//# sourceMappingURL=Adapter.js.map
